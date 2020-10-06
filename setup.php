8203895f027637f371eeda483de5470f75b2a197
Aminu M. Bulangu <42899653+blgguy@users.noreply.github.com>
2020-10-06 07:33:57 -0700

    Create setup.php

---
+++ setup.php
@@ -0,0 +1,17 @@
+<?php
+// Setup Firestore with php
+// composer require "grpc/grpc:^v1.27.0" || composer require google/cloud-firestore
+/**
+ * short ling ref= (https://cloud.google.com/php/grpc?authuser=0#linux-macos)
+ * composer require "grpc/grpc:^v1.27.0" || composer require google/cloud-firestore
+ * then
+ * https://pecl.php.net/package/grpc = to download the .dll file for your processor nts
+ * for windows
+ * Add this line anywhere in your php.ini file, for example, C:\Program Files\PHP\7.3\php.ini.
+ * extension=php_grpc.dll
+ * for linus
+ * Add this line anywhere in your php.ini file, for example, /etc/php7/cli/php.ini. You can find this file by running php --ini.
+ * extension=grpc.so
+ * 
+ */
+?>
